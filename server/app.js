f24593fae3ae2c93ccc2ab895b3aca04ed8a2392
keki94 <quequisegarra@gmail.com>
2021-07-20 19:17:33 +0100

    app serve to routes POST/posts GET/posts/:id

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const Post = require('./models/post');
 const app = express();
 
 app.use(express.json());
@@ -10,6 +10,24 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
 	res.status(200).send('Hello World!');
+});
+
+app.post('/posts', async (req, res) => {
+	try{
+		const post = await Post.create(req.body);
+        res.status(201).json(post);
+	} catch (err) {
+		res.status(422).send(err);
+	}
+});
+
+app.get('/posts/:id', async (req, res) => {
+	try{
+		const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+	} catch (err) {
+		res.status(404).send(err);
+	}
 });
 
 module.exports = app;
