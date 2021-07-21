d446dd66f4b10a8ccaa7a5ca45d78c9f8227cede
keki94 <quequisegarra@gmail.com>
2021-07-21 17:41:41 +0100

    new init configuration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4371;
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
