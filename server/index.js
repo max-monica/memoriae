3769f110420e34cadc2c7caf006a487dfc49636c
keki94 <quequisegarra@gmail.com>
2021-07-22 09:47:04 +0100

    changing useless tweeks

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 4371;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
--- server/models/post.js
+++ server/models/post.js
@@ -1,4 +1,4 @@
-const db = require('../dbconfig/init.js');
+const db = require('../dbconfig/init');
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = class Post {
